6dcf0ee2fc71f507c07d1afe14e2f5ad697f9dcf
KDS-Limitless <limitlesskds@gmail.com>
2021-04-23 16:36:30 +0100

    Login authorization

--- DJANGO/Zblog/forms.py
+++ DJANGO/Zblog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,4 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email"]
+
--- DJANGO/Zblog/views.py
+++ DJANGO/Zblog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterForm
-
 
 
 def register_user(request) -> RegisterForm:   
@@ -10,7 +8,7 @@ def register_user(request) -> RegisterForm:
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            name = form.cleaned_data.get('firstName')
+            name = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {name}!')
             return redirect('/login/')
         error = form.errors
@@ -19,6 +17,5 @@ def register_user(request) -> RegisterForm:
     return render(request, 'register.html', {"form": form})
 
 
-def login_user(request) -> RegisterForm:
-    login_form = AuthenticationForm()
-    return render(request, "login.html", {"login_form": login_form})
+def index(request):
+    return render(request, "index.html")
